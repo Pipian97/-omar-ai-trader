999ffcc6ae1c09af2145eafbb9514bcb527b7767
Pipian97 <omargcuriel97@gmail.com>
2026-09-14 14:33:39 -0500

    Add submit_paper_bracket import to app.py

--- app.py
+++ app.py
@@ -7,8 +7,8 @@ from market_data import fetch_bars
 from indicators import add_indicators
 from strategy import score_latest
 from backtest import run_backtest
-from risk import position_size
-from broker import get_clients
+from risk import position_size  
+from broker import get_clients, submit_paper_bracket
 from scanner import scan_market
 
 st.set_page_config(page_title="OMAR AI TRADER", layout="wide")
